14cd2d895534ffcdc3e364699afc981667e3bcaf
danilogc4 <danilogc4@gmail.com>
2022-06-24 00:33:38 -0300

    Update GameMode.java

--- src/GameModes/GameMode.java
+++ src/GameModes/GameMode.java
@@ -3,17 +3,23 @@ package GameModes;
 import java.util.ArrayList;
 
 import Utilities.Color;
+import Utilities.Player;
 
 public abstract class GameMode {
     protected ArrayList<Color> sequence;
     protected int round;
+    protected Player currentPlayer;
 
-    public GameMode() {
+    public GameMode(Player currentPlayer) {
         this.round = 0;
+        this.currentPlayer = currentPlayer;
+        this.sequence = new ArrayList<Color>();
     }
 
-    public abstract void run();
+    public abstract void run(int difficulty) throws InterruptedException;
     public abstract void update();
+    protected abstract void defeat();
+    protected abstract void win();
     protected void printSequence() throws InterruptedException{
         for(Color color : sequence){
             System.out.printf(color.getValue() + " - " +color.getDescription());
@@ -23,7 +29,7 @@ public abstract class GameMode {
 
     }
 
-    private void clearConsole(){
+    protected void clearConsole(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
