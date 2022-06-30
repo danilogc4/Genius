f9e694a18926c9210d8c17f144248d22683e97fa
danilogc4 <danilogc4@gmail.com>
2022-06-30 01:21:52 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
-import GameModes.RepeatSequence;
-import Utilities.Player;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Player player = new Player("Danilo");
-        RepeatSequence repeatSequence = new RepeatSequence(player);
-        Game game = new Game(repeatSequence, 4);
-
-        game.play();
+        Game game = new Game();
+        game.showMenu();
     }
 }
